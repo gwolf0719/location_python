8c9afec340f35389ab119551dcb1d843b546ed85
James <james@fofo.tw>
2018-05-08 09:48:54 +0800

    硬體回傳原始資料

--- flaskapp.py
+++ flaskapp.py
@@ -1,45 +1,36 @@
 #coding=utf-8
 #flaskapp.py
-import sys
 from flask import Flask,abort
 from flask import jsonify
 from flask import request
-# from flask import pymongo
-# from pymongo import MongoClient
+
 from flask_pymongo import PyMongo
-# from pymongo import MongoClient
 
 app = Flask(__name__)
 
- 
-
 app.config['MONGO_HOST'] = 'localhost'
 app.config['MONGO_PORT'] = '27017'
-app.config['MONGO_DBNAME'] = 'test'
-# app.config['MONGO_USERNAME'] = 'root'
-# app.config['MONGO_PASSWORD'] = 'aaa2016'
-# app.config['MONGO_AUTH_SOURCE'] = 'admin' 
+app.config['MONGO_DBNAME'] = 'location'
 mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return '111'
+    return 'Hello Flask Apps'
 
-@app.route('/test/', methods=['POST'])
-def api_tasks():
-    if request.method == 'POST':
-        data = request.get_json()
-        if not data:
-            return 'No json data found', 400
 
-        collection = mongo.db.test_coll
-        result = {
-            'json data in request': data['title']
-        }
-        collection.insert({'name': 'name', 'distance': 'distance'})
-        return jsonify(result)
+# 硬體回傳原始資料開始
+@app.route('/api_setlocation/',methods=['POST'])
+def api_setlocation():
+    data = request.get_json()
+    col = mongo.db.raw_data
+    col.insert(data)
+    result = {
+        'sys_code':'200',
+        'sys_msg':'success'
+    }
+    return jsonify(result)
+# 硬體回傳原始資料結束
 
-    return 'Please post json data'
 
 
 if __name__ == '__main__':
