1fa84d317f2782e6ee505a4fbd6060aed43e7b84
James <james@fofo.tw>
2018-05-15 18:00:56 +0800

    aaaaa

--- flaskapp.py
+++ flaskapp.py
@@ -29,6 +29,18 @@ def api_setlocation():
         'sys_code':'200',
         'sys_msg':'success'
     }
+    #分散存檔
+    detector_id = data.detector_id;
+    time = data.time;
+    for d in data.beacons:
+        new_data = {
+            "beacon_id":d.beacon_id,
+            "beacon_rssi":d.beacon_rssi,
+            "detector_id":detector_id,
+            "time":time,
+        }
+        mongo.db.locate_list.insert(new_data)
+
     return jsonify(result)
 # 硬體回傳原始資料結束
 
@@ -38,6 +50,23 @@ def listlocation():
     output = [];
     for q in col.find().sort('time',-1).limit(50):
         output.append({'time':q['time'],'detector_id':q['detector_id']})
+    return jsonify({'result':output})
+
+@app.route('/detector_list/')
+def detector_list():
+    col = mongo.db.raw_data
+    output = [];
+
+    for q in col.aggregate([{"$group" : {"_id" : "$detector_id",'count': {'$sum': 1}}}]):
+        output.append({'detector_id':q['_id'],'sum':q['count']})
+    return jsonify({'result':output})
+
+@app.route('/beacon_list/')
+def beacon_list():
+    col = mongo.db.raw_data
+    output = [];
+    for q in col.aggregate([{"$group" : {"_id" : "$beacon_list",'count': {'$sum': 1}}}]):
+        output.append({'beacon_list':q['_id'],'sum':q['count']})
     return jsonify({'result':output})
 
 
